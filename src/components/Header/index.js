4a84e9ec3e62ece7f7ca59a1bc4c707305058e1d
Jason <jasonpsmith180@gmail.com>
2021-07-03 20:52:33 -0600

    fixed link issue

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,7 +11,7 @@ function Header(props) {
     return (
         <header className="flex-row px-1">
             <h2>
-                <a href='/'>
+                <a href='/react-profile'>
                     Jason Smith
                 </a>
             </h2>
