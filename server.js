dc7c994b7557a9b35d1db4121d8047b78ce1b649
Jason <jasonpsmith180@gmail.com>
2021-10-24 16:04:31 -0600

    heroku deploy

--- server.js
+++ server.js
@@ -49,6 +49,14 @@ app.post("/send", function (req, res) {
     });
 });
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
