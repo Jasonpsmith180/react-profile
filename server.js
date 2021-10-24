49d33c4af10d92a6d61423aebd0a5316dd64adab
Jason <jasonpsmith180@gmail.com>
2021-10-24 16:09:35 -0600

    heroku deploy

--- server.js
+++ server.js
@@ -50,11 +50,11 @@ app.post("/send", function (req, res) {
 });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '/build')));
 }
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
